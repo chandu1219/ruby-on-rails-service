1d4c412b977f71e48116b902e8112a21522c8b4c
Chandrasekhar Koripella <chandra.koripella@amzurtech.com>
2019-03-26 12:02:50 +0530

    cap changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,45 +1,49 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+# config valid only for current version of Capistrano
+lock '3.11.0'
 
-set :application, "ruby-on-rails-service"
-set :repo_url, "git@github.com:chandu1219/ruby-on-rails-service.git"
-
+set :application, 'ruby-on-rails-service'
+set :repo_url, 'git@github.com:chandu1219/ruby-on-rails-service.git'
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "set :deploy_to, "/home/ubuntu/#{fetch(:application)}""
+#set :deploy_to, "/var/www/#{fetch(:application)}"
+set :deploy_to, "/home/ubuntu/#{fetch(:application)}"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+# Default value for :scm is :git
+# set :scm, :git
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-set :passenger_restart_options, -> { "#{deploy_to} --ignore-app-not-running --rolling-restart" }
-set :passenger_roles, :web
-
-set :rvm_ruby_version, '2.3.0'
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+# There is a known bug that prevents sidekiq from starting when pty is true
+set :pty, false
+
+# Override the basic sidekiq cmd with out own helper
+# This is needed because Sidekiq requires Rails otherwise
+#set :sidekiq_cmd, "bin/sidekiq"
+#set :passenger_restart_options, -> { "#{deploy_to} --ignore-app-not-running --rolling-restart" }
+set :passenger_roles, :web
+set :passenger_restart_command, 'passenger-config restart-app'
+set :passenger_restart_options, -> { "#{deploy_to} --ignore-app-not-running" }
+
 namespace :deploy do
 
   after :restart, :clear_cache do
@@ -50,5 +54,4 @@ namespace :deploy do
       # end
     end
   end
-
 end
